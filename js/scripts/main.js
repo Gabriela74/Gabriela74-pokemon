f5a53f258370e829163a49d0201ae3340ae26267
Gabriela 74 <gabrielaacostta74@gmail.com>
2023-01-31 09:52:37 -0300

    header+section-slide-hero

---
+++ js/scripts/main.js
@@ -0,0 +1,26 @@
+// scripts do slide principal
+var slide_hero = new Swiper(".slide-hero", {
+  effect: 'fade',
+  pagination: {
+    el: " .slide-hero .main-area .area-explore .swiper-pagination",
+  },
+});
+
+
+const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
+const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon');
+
+function openDetailsPokemon() {
+  document.documentElement.classList.add('open-modal');
+}
+
+function closeDetailsPokemon() {
+  document.documentElement.classList.remove('open-modal');
+}
+
+
+cardPokemon.forEach(card => {
+  card.addEventListener('click' , openDetailsPokemon);
+})
+
+btnCloseModal.addEventListener('click' , closeDetailsPokemon);
